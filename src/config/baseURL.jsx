40e2a8fca0f231f1f0e1fe0855022c8f39462894
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-08-18 07:28:46 +0100

    Fixed a bug

--- src/config/baseURL.jsx
+++ src/config/baseURL.jsx
@@ -2,6 +2,6 @@
 //   ? "http://localhost:5005/api/v1"
 //   : "https://mytownmart-api.herokuapp.com/api/v1";
 
-export default true
+export default !true
   ? "http://localhost:5005/api"
   : "https://stech-api.herokuapp.com/api";
